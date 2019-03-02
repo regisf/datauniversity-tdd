ae48a7a3e5e7455274795489e1263536a5e648dc
Regis FLORET <regisfloret@protonmail.com>
2019-03-02 11:44:11 +0100

    Fix . at the end of the regex

--- validate.py
+++ validate.py
@@ -2,5 +2,5 @@ import re
 
 
 def validate_ip_address(ip_address):
-    return re.match(r'\d+\.\d+\.\d+\.\d+\.', ip_address) is not None    
+    return re.match(r'\d+\.\d+\.\d+\.\d+', ip_address) is not None    
     
